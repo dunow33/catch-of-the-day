d70f9cfacbd6f816db01e2ae72fb96e9d395e854
Brian Donnoe <dunow33@hotmail.com>
2019-05-19 12:08:26 -0400

    finished video 17

--- src/components/Order.js
+++ src/components/Order.js
@@ -7,9 +7,14 @@ class Order extends React.Component {
 
 		const fish = this.props.fishes[key];
 		const count = this.props.order[key];
+		const isAvailable = fish.status === 'available';
+
+		if(!isAvailable) {
+			return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
+		}
 
 		return (
-				<li>
+				<li key={key}>
 					{count} lbs {fish.name}
 					{formatPrice(count * fish.price)}
 				</li>
@@ -31,7 +36,7 @@ class Order extends React.Component {
 		return (
 			<div className="order-wrap">
 				<h2>Order</h2>
-				<ul>
+				<ul className="order">
 					{orderIds.map(this.renderOrder)}
 				</ul>
 				<div className="total">
