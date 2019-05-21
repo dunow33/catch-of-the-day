d42a139511921af4b52d56c49215146188df4e65
Brian Donnoe <dunow33@hotmail.com>
2019-05-21 01:57:48 -0400

    finished video 21.....removing items from state

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -39,6 +39,7 @@ class EditFishForm extends React.Component {
 						name="image" 
 						onChange={this.handleChange} 
 						value={this.props.fish.image} />
+				<button onClick={() => this.props.deleteFish(this.props.index)}>Remove Fish</button>
 			</div>
 			);
 	}
